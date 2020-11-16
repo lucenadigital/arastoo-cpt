b2fe8fc0a68c050c972df60a4dbb5fdac15141ff
Michael <61347143+lucenadigital@users.noreply.github.com>
2020-11-16 11:42:23 +0800

    Add files via upload

---
+++ parts/cpt-register.php
@@ -0,0 +1,127 @@
+ <?php 
+// REGISTERING CUSTOM POST TYPES
+// BLOCK MANAGEMENT
+//OUR TEAM
+
+if ( ! function_exists( 'create_post_type' ) ) :
+ function create_post_type() {
+
+	$arastoo_cpt_title = get_option('arastoo_cpt_title');
+	$arastoo_cpt_title_two = get_option('arastoo_cpt_title_two');
+	$arastoo_cpt_slug = get_option ('arastoo_cpt_slug');
+	$arastoo_cpt_slug_two = get_option ('arastoo_cpt_slug_two');
+
+		if (empty($arastoo_cpt_title)) {
+			//do something if empty
+			} else {
+			//do something else if not empty
+			register_post_type( $arastoo_cpt_title, // change the name
+				array(
+					'labels' => array(
+						'name' => __( $arastoo_cpt_title ), // change the name
+						'singular_name' => __( $arastoo_cpt_title ), // change the name
+					),
+					'public' => true,
+					'supports' => array ( 'title', 'editor', 'custom-fields', 'page-attributes', 'thumbnail' ), // extra options
+					//'taxonomies' => array( 'category', 'post_tag' ), //  categories and tags
+					'hierarchical' => true,
+					'menu_icon' => 'dashicons-admin-page',
+					'rewrite' => array ( 'slug' => __( $arastoo_cpt_slug ) ) // change the name
+				)
+			);
+			}
+
+		if (empty($arastoo_cpt_title_two)) {
+			//do something if empty
+			} else {
+			//do something else if not empty
+			register_post_type( $arastoo_cpt_title_two, // change the name
+				array(
+					'labels' => array(
+						'name' => __( $arastoo_cpt_title_two ), // change the name
+						'singular_name' => __( $arastoo_cpt_title_two ), // change the name
+					),
+					'public' => true,
+					'supports' => array ( 'title', 'editor', 'page-attributes', 'thumbnail' ), // extra options
+					//'taxonomies' => array( 'category', 'post_tag' ), //  categories and tags
+					'hierarchical' => true,
+					'menu_icon' => 'dashicons-admin-page',
+					'rewrite' => array ( 'slug' => __( $arastoo_cpt_slug_two ) ) // change the name
+				)
+			);
+			}
+}
+
+endif; //
+
+function arastoo_cpt_group_settings() {
+ 
+	register_setting('arastoo_cpt_options_group', 'arastoo_cpt_title');
+ 	register_setting('arastoo_cpt_options_group', 'arastoo_cpt_slug');
+	register_setting ('arastoo_cpt_options_group', 'arastoo_cpt_title_two');
+	register_setting ('arastoo_cpt_options_group', 'arastoo_cpt_slug_two');
+ 
+}
+
+function arastoo_cpt_options() {
+ 
+	add_options_page(
+		'Arastoo CPT', // page <title>Title</title>
+		'Arastoo CPT', // menu link text
+		'manage_options', // capability to access the page
+		'arastoo-cpt', // page URL slug
+		'arastoo_cpt_options_content', // callback function with content
+		2 // priority
+	);
+ 
+}
+
+function arastoo_cpt_options_content() { ?>
+ 
+<div class="wrap">
+        <h2>Arastoo CPT</h2>
+        <form method="post" action="options.php">
+            <?php settings_fields('arastoo_cpt_options_group'); ?>
+ 
+        <table class="form-table">
+ 
+            <tr>
+                <th><label for="arastoo_cpt_title_id">CPT Title:</label></th>
+ 
+                <td>
+<input type = "text" class="regular-text" id="arastoo_cpt_title_id" name="arastoo_cpt_title" value="<?php echo get_option('arastoo_cpt_title'); ?>"> <button type="button" onclick="ClearCPTtitle1();">Clear</button> <em id="alert-one" style="color:#F00;"></em>
+                </td>
+            </tr>
+ 
+            <tr>
+                <th><label for="arastoo_cpt_slug_id">CPT Slug:</label></th>
+                <td>
+<input type = "text" class="regular-text" id="arastoo_cpt_slug_id" name="arastoo_cpt_slug" value="<?php echo get_option('arastoo_cpt_slug'); ?>">
+                </td>
+            </tr>
+ 
+            <tr>
+                <th><label for="arastoo_cpt_title_two_id">CPT Title 2:</label></th>
+   <td>
+<input type = "text" class="regular-text" id="arastoo_cpt_title_two_id" name="arastoo_cpt_title_two" value="<?php echo get_option('arastoo_cpt_title_two'); ?>"> <button type="button" onclick="ClearCPTtitle2();">Clear</button> <em id="alert-two" style="color:#F00;"></em>
+                </td>
+            </tr>
+            <tr>
+                <th><label for="arastoo_cpt_slug_two_id">CPT Slug 2:</label></th>
+                <td>
+<input type = "text" class="regular-text" id="arastoo_cpt_slug_two_id" name="arastoo_cpt_slug_two" value="<?php echo get_option('arastoo_cpt_slug_two'); ?>">
+                </td>
+            </tr>
+        </table>
+ <div><em><strong style="color:#F00;">IMPORTANT:</strong> Update permalink structure after clicking on the Save Changes button.</em> Go to Settings > <a href="<?php echo admin_url( 'options-permalink.php' ); ?>">Permalinks</a></div>
+        <?php submit_button(); ?>
+    </form>
+    </div>
+	<div>
+
+
+	</div>
+
+<?php } 
+
+?>
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Arastoo Custom Post Type ===
+Contributors: Arteo
+Tags: custom post type, cpt, simple custom post type, easy
+Donate link:  https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=SFB86DRUEMPGC&currency_code=USD&amount=5
+Requires at least: 4.7
+Tested up to: 5.4
+Requires PHP: 7.0
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+WordPress Custom Post Type 
+This plugin can help you create upto two custom post types.  
+
+=== Instruction ===
+1. Upload Arastoo Custom Post Type to your blog.
+2. Install
+3. Activate
+4. Create custom post type under Settings > Arastoo CPT
+
+== Frequently Asked Questions ==
+= 1. How many custom post type can I add? =
+You can add up to Two custom post type
+
+== Screenshots ==
+* arastoo-cpt-options-settings.png - Options settings 
+
+== Changelog ==
+= V1.0.1 = 
+* Seperation of parts and assets.
+
+= V1.0 =
+*Initial developent.
+
+== Upgrade Notice ==
+
+= V1.0 - 16.11.2020 =
+* Seperation of parts and assets.
+
+= V1.0 - 13.11.2020 =
+* Update : Immediate upgrade.
+
