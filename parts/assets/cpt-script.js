80821ef8fbf56d4cb5f380003c699953e0aef961
Michael <61347143+lucenadigital@users.noreply.github.com>
2020-11-23 07:33:19 +0800

    Update cpt-script.js

--- parts/assets/cpt-script.js
+++ parts/assets/cpt-script.js
@@ -1,10 +1,9 @@
-
-	function ClearCPTtitle1() {
+function ClearCPTtitle1() {
      document.getElementById("arastoo_cpt_title_id").value = "";
 	 document.getElementById("alert-one").innerHTML = "Don't forget to SAVE your changes!";
 	}
 	
-	function ClearCPTtitle2() {
+function ClearCPTtitle2() {
      document.getElementById("arastoo_cpt_title_two_id").value = "";
 	 document.getElementById("alert-two").innerHTML = "Don't forget to SAVE your changes!";
 	}
