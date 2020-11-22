df182588dcf749f2f409868150c292a35c51ba62
Michael <61347143+lucenadigital@users.noreply.github.com>
2020-11-22 15:14:46 +0800

    Update arastoo-cpt.php

--- arastoo-cpt.php
+++ arastoo-cpt.php
@@ -1,24 +1,23 @@
 <?php
 /**
  * @package Arastoo CPT
- * @version 1.0.1
+ * @version 1.0.2
  */
 /*
 Plugin Name: Arastoo CPT
 Plugin URI: https://github.com/lucenadigital/arastoo-cpt/
 Description: Smart Plugin that can create upto two Custom Post Type. Settings > Arastoo CPT
 Author: Arteo
-Version: 1.0.1
-Author URI: https://github.com/lucenadigital/
+Version: 1.0.2
+Author URI: https://www.michaelcastrillo.com/
 */
 
+/** Include parts & assets files **/
+include plugin_dir_path( __FILE__ ) . 'parts/arastoo_cf_functions.php';
+include plugin_dir_path( __FILE__ ) . 'assets/cpt-script.js';
 
-$plugin_dir = ABSPATH . 'wp-content/plugins/arastoo-cpt/';
-
-include $plugin_dir . 'assets/cpt-script.js';
-include $plugin_dir . 'parts/cpt-register.php';
-
-add_action( 'init', 'arastoo_create_post_type' );
+/** Initiate functions **/
+add_action( 'init', 'arastoo_cpt_create_main' );
 add_action('admin_init', 'arastoo_cpt_group_settings');
 add_action( 'admin_menu', 'arastoo_cpt_options' );
 
